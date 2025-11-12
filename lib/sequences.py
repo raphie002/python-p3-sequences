78d175715155ffa320753e876ded422089621a92
Raphael Njoroge <raphael.njoroge@student.moringaschool.com>
2025-11-12 23:58:08 +0300

    First commit

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length <= 0:
+        fib_list = []
+    elif length == 1:
+        fib_list = [0]
+    else:
+        fib_list = [0, 1]
+        
+        for i in range(2, length):
+            next_fib = fib_list[i-1] + fib_list[i-2]
+            fib_list.append(next_fib)
+
+    print(str(fib_list))
